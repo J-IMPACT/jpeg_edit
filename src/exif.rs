8b7408af6d542046fa7f4b10d9b5553fa6ee5724
Shuntaro Ohno <sohno@ushitora.net>
2026-01-30 09:31:03 +0900

    edit exif

--- src/exif.rs
+++ src/exif.rs
@@ -4,6 +4,14 @@ pub fn ur64_to_f64(u: &uR64) -> f64 {
     u.nominator as f64 / u.denominator as f64
 }
 
+pub fn dir_to_f64(s: &str) -> Option<f64> {
+    match s {
+        "N" | "E" => Some(1.0),
+        "S" | "W" => Some(-1.0),
+        _ => None,
+    }
+}
+
 pub fn dms_to_f64(v: &[uR64]) -> Option<f64> {
     if v.len() != 3 { return None; }
     Some(ur64_to_f64(&v[0]) + ur64_to_f64(&v[1]) / 60.0 + ur64_to_f64(&v[2]) / 3600.0)
@@ -21,14 +29,6 @@ pub fn f64_to_dms(value: f64) -> Option<Vec<uR64>> {
         uR64 { nominator: v[1], denominator: 1 },
         uR64 { nominator: v[2] * 100, denominator: 100 }
     ])
-}
-
-pub fn dir_to_f64(s: &str) -> Option<f64> {
-    match s {
-        "N" | "E" => Some(1.0),
-        "S" | "W" => Some(-1.0),
-        _ => None,
-    }
 }
 
 pub fn latlng_to_exif(lat: f64, lng: f64) -> (String, Vec<uR64>, String, Vec<uR64>) {
